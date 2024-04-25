6b5524a87ef7deab8cdabfc04347a0cb92a4742e
Adam1277 <adam.levere@ontariotechu.net>
2024-04-25 14:34:24 -0400

    Added most boilerplate and some functionality of the frontend

---
+++ DataStructresClient/js/main.js
@@ -0,0 +1,31 @@
+
+
+function nextStep(){
+
+}
+
+function stacks(){
+    // To initialize some basics on the Stack Data Structure
+    let area = document.getElementById("text-area");
+    area.textContent = "Stacks use a FIFO (First in first out) procedure with accompanying nodes. The first subsequent node to be placed" + "on the stack will be the first node out";
+
+    // Now to create boxes for the implementation
+    let box = document.getElementById("box");
+    box.style.background = "grey";
+    box.style.borderColor = "black";
+    box.style.borderWidth = "20px";
+
+
+}
+
+function queues(){
+
+}
+
+function linkedList(){
+
+}
+
+function binaryTree(){
+
+}
